1e267f29976bca1dbc70388e120df15e1a46a079
Tom Cubie <tom@radxa.com>
2014-03-25 14:43:42 +0800

    enable status led1(green) when kernel booting up

--- arch/arm/mach-rk3188/board-rk3188-box.c
+++ arch/arm/mach-rk3188/board-rk3188-box.c
@@ -175,6 +175,7 @@ static int rk29_backlight_io_deinit(void)
 	return ret;
 }
 
+
 static int rk29_backlight_pwm_suspend(void)
 {
 	int ret = 0, pwm_gpio;
@@ -189,6 +190,9 @@ static int rk29_backlight_pwm_suspend(void)
 	gpio_direction_output(BL_EN_PIN, 0);
 	gpio_set_value(BL_EN_PIN, !BL_EN_VALUE);
 #endif
+
+	gpio_direction_output(RADXA_STATUS_LED1, GPIO_HIGH);
+
 	return ret;
 }
 
@@ -203,6 +207,9 @@ static int rk29_backlight_pwm_resume(void)
 	gpio_direction_output(BL_EN_PIN, 1);
 	gpio_set_value(BL_EN_PIN, BL_EN_VALUE);
 #endif
+
+	gpio_direction_output(RADXA_STATUS_LED1, GPIO_LOW);
+
 	return 0;
 }
 
@@ -2290,6 +2297,10 @@ static void rk30_pm_power_off(void)
 	while (1);
 }
 
+/* led1: green, led2: blue */
+#define RADXA_STATUS_LED1 RK30_PIN0_PB4
+#define RADXA_STATUS_LED2 RK30_PIN0_PB6
+
 static void __init machine_rk30_board_init(void)
 {
 	avs_init();
@@ -2300,6 +2311,8 @@ static void __init machine_rk30_board_init(void)
 	
         gpio_direction_output(POWER_ON_PIN, GPIO_HIGH);
 
+	gpio_request(RADXA_STATUS_LED1, "led1");
+	gpio_direction_output(RADXA_STATUS_LED1, GPIO_LOW);
 
 	rk30_i2c_register_board_info();
 	spi_register_board_info(board_spi_devices, ARRAY_SIZE(board_spi_devices));
