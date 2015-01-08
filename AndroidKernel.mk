49ad2b52aa948f5c0127aab2e202a8b4f6ecc08e
AZ technology <aztec@localhost.domain>
2015-01-08 11:10:04 +0800

    Change CROSS_COMPILE to arm-eabi-4.6

--- AndroidKernel.mk
+++ AndroidKernel.mk
@@ -3,10 +3,10 @@ KERNEL_CONFIG := $(KERNEL_OUT)/.config
 TARGET_PREBUILT_KERNEL := $(KERNEL_OUT)/arch/arm/boot/Image
 
 $(TARGET_PREBUILT_KERNEL) : $(KERNEL_OUT) $(KERNEL_CONFIG)
-	$(MAKE) -C kernel ARM=arm CROSS_COMPILE=arm-eabi- O=../$(KERNEL_OUT) kernel.img
+	$(MAKE) -C kernel ARM=arm CROSS_COMPILE=$(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-eabi-4.6/bin/arm-eabi- O=../$(KERNEL_OUT) kernel.img
 
 $(KERNEL_CONFIG) : $(KERNEL_OUT)
-	$(MAKE) -C kernel ARM=arm CROSS_COMPILE=arm-eabi- O=../$(KERNEL_OUT) $(KERNEL_DEFCONFIG)
+	$(MAKE) -C kernel ARM=arm CROSS_COMPILE=$(ANDROID_BUILD_TOP)/prebuilts/gcc/linux-x86/arm/arm-eabi-4.6/bin/arm-eabi- O=../$(KERNEL_OUT) $(KERNEL_DEFCONFIG)
 
 $(KERNEL_OUT) :
 	mkdir -p $(KERNEL_OUT)
